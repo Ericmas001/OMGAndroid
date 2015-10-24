533ad2fd844c312e52d992981083ca803d35043d
Éric Massé <Ericmas001@gmail.com>
2015-10-24 07:43:42 -0400

    Adding splashscreen

---
+++ app/src/main/java/com/ericmas001/omgandroid/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.ericmas001.omgandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
